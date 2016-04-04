46252ec5fb6eb0df7b6cfcd2ed0ab86b0ee5ee56
Bradford Knowlton <brad@ucanbonline.net>
2016-04-04 12:40:58 -0400

    Version Bump

--- dm-gravity-banner.php
+++ dm-gravity-banner.php
@@ -6,7 +6,7 @@ Description: Adds custom banner before gravity forms.
 Plugin URI: https://github.com/DesignMissoula/DM-gravity-banner
 Author: Bradford Knowlton
 Author URI: http://bradknowlton.com
-Version: 1.2.2
+Version: 1.2.3
 License: GPL2
 Text Domain: hwd
 Domain Path: /languages
